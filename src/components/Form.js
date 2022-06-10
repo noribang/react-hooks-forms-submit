00919f5db5cb07098594bd4d5d977bea582b0989
hoonkim <hoonk9387@gmail.com>
2022-06-09 21:11:07 -0500

    Added to handleSubmit. Created state submittedData. Call .map() on submittdData.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 
 function Form() {
+  /* Create state. State value. State setter function. */
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
 
+  /* Event handlers passed as props. */
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
-
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
@@ -21,6 +23,9 @@ function Form() {
       firstName: firstName,
       lastName: lastName
     } 
+    const dataArray = [...submittedData, formData];
+    console.log(dataArray)
+    setSubmittedData(dataArray);
     // Send form state values from object.
     // props.sendFormDataSomewhere(formData);
     
@@ -29,13 +34,25 @@ function Form() {
     setLastName("");
   }
 
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    /* Event listener. */
-    <form onSubmit={handleSubmit}>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        {/* Event listener. Event handler. State value. */}
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
